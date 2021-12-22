7c96917c6d5af4ab40288e664f36929d4528fbc2
Ben Verbeken <ben@seats.io>
2021-12-22 10:27:16 +0100

    initial import

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { SeatsioVueSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  SeatsioVueSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <seatsio-vue-sample />
+  </div>
+</template>
