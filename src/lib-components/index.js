577c927d459b339c4dc2d08ba654e7933f0ead45
Morten Devold <morten@mortens-mbp.lan>
2023-02-08 11:52:56 +0100

    Export new Vue components

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,2 +1,5 @@
 /* eslint-disable import/prefer-default-export */
+export { default as seatsioChartManager } from './seatsioChartManager.vue';
+export { default as seatsioDesigner } from './seatsioDesigner.vue';
+export { default as seatsioEventManager } from './seatsioEventManager.vue';
 export { default as seatsioSeatingChart } from './seatsioSeatingChart.vue';
