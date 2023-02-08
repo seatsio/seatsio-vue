cb442002405bae956519fc231593f939c7a55816
Morten Devold <morten@mortens-mbp.lan>
2023-02-08 11:53:56 +0100

    Add compoentRenderer to switch between chart types in dev app

---
+++ dev/componentRenderer.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: 'ComponentRenderer',
+  methods: {
+    onClick: function(selection) {
+        this.$emit('selectChartType', selection)
+    }
+  }
+}
+</script>
+
+<template>
+  <ul id="menu">
+      <li><button @click="onClick('seatingChart')">Seating Chart</button></li>
+      <li><button @click="onClick('chartManager')">ChartManager</button></li>
+      <li><button @click="onClick('chartDesigner')">Designer</button></li>
+      <li><button @click="onClick('eventManager')">EventManager</button></li>
+    </ul>
+</template>
+
+<style scoped>
+#menu {
+  list-style-type: none;
+  display: inline;
+}
+
+#menu > li {
+  display: inline;
+  margin-right: 4px;
+}
+</style>
--- dev/serve.vue
+++ dev/serve.vue
@@ -1,25 +1,42 @@
 <script>
-import Vue from 'vue';
-import SeatsioSeatingChart from "@/lib-components/seatsioSeatingChart";
+import Vue from 'vue'
+import ComponentRenderer from './componentRenderer.vue'
+import SeatsioSeatingChart from '@/lib-components/seatsioSeatingChart.vue'
+import SeatsioChartManager from '@/lib-components/seatsioChartManager.vue'
+import SeatsioDesigner from '@/lib-components/seatsioDesigner.vue'
+import SeatsioEventManager from '@/lib-components/seatsioEventManager.vue'
 
 export default Vue.extend({
   name: 'ServeDev',
-  components: {SeatsioSeatingChart},
+  components: {
+    ComponentRenderer,
+    SeatsioChartManager,
+    SeatsioDesigner,
+    SeatsioEventManager,
+    SeatsioSeatingChart,
+  },
   data: () => ({
     messages: { clickToSelect: 'Click to pick'},
     objectColor: () => 'grey',
-    tooltipInfo: () => 'My custom info'
+    tooltipInfo: () => 'My custom info',
+    selectedComponent: 'seatingChart'
   }),
   methods: {
-    onRenderStarted: chart => console.log("Render started", chart),
-    onObjectClicked: clickedObject => console.log("Object clicked", clickedObject)
+    onRenderStarted: chart => console.log('Render started', chart),
+    onObjectClicked: clickedObject => console.log('Object clicked', clickedObject),
+    // Dev purposes only
+    selectChartType: function (selection) {
+      this.selectedComponent = selection
+    }
   },
 });
 </script>
 
 <template>
   <div id="app">
+    <ComponentRenderer @selectChartType="selectChartType" />
     <SeatsioSeatingChart
+        v-if="selectedComponent === 'seatingChart'"
         id="myChart"
         workspaceKey="publicDemoKey"
         event="smallTheatreEvent"
@@ -30,11 +47,45 @@ export default Vue.extend({
         @onRenderStarted="onRenderStarted"
         @onObjectClicked="onObjectClicked"
     />
+    <SeatsioChartManager
+        v-if="selectedComponent === 'chartManager'"
+        workspaceKey="publicDemoKey"
+        mode="manageRulesets"
+        :messages="messages"
+        :tooltipInfo="tooltipInfo"
+        @onRenderStarted="onRenderStarted"
+        @onObjectClicked="onObjectClicked"
+    />
+    <SeatsioDesigner
+        v-if="selectedComponent === 'chartDesigner'"
+        event="[Your event key]"
+        id="myChartDesigner"
+        workspaceKey="publicDemoKey"
+        mode="readOnly"
+        :messages="messages"
+        :tooltipInfo="tooltipInfo"
+        @onRenderStarted="onRenderStarted"
+        @onObjectClicked="onObjectClicked"
+        chartKey="[Your chart key]"
+    />
+    <SeatsioEventManager
+        v-if="selectedComponent === 'eventManager'"
+        id="myChartDesigner"
+        workspaceKey="publicDemoKey"
+        mode="manageRulesets"
+        :messages="messages"
+        :tooltipInfo="tooltipInfo"
+        @onRenderStarted="onRenderStarted"
+        @onObjectClicked="onObjectClicked"
+    />
   </div>
 </template>
 
 <style scoped>
 #myChart {
   height: 600px;
+}
+#myChartDesigner {
+  height: 100vh
 }
 </style>
