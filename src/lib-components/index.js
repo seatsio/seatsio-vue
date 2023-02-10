5c827613d3f0f031058bc1a60a24e8438b3ab76f
Morten Devold <morten@seats.io>
2023-02-10 08:39:34 +0100

    Pascal case component exports

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-export { default as seatsioChartManager } from './seatsioChartManager.vue';
-export { default as seatsioDesigner } from './seatsioDesigner.vue';
-export { default as seatsioEventManager } from './seatsioEventManager.vue';
-export { default as seatsioSeatingChart } from './seatsioSeatingChart.vue';
+export { default as SeatsioChartManager } from './seatsioChartManager.vue';
+export { default as SeatsioDesigner } from './seatsioDesigner.vue';
+export { default as SeatsioEventManager } from './seatsioEventManager.vue';
+export { default as SeatsioSeatingChart } from './seatsioSeatingChart.vue';
