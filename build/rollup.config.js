03890fc607ca73215081d8b68367bd788e10a79f
Morten Devold <morten@seats.io>
2023-11-20 18:31:31 +0100

    Fix build

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -9,6 +9,7 @@ import replace from '@rollup/plugin-replace';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import minimist from 'minimist';
+import typescript from 'rollup-plugin-typescript2';
 
 // Get browserslist config and remove ie from es build targets
 const esbrowserslist = fs.readFileSync('./.browserslistrc')
@@ -67,6 +68,7 @@ const external = [
   // list external dependencies, exactly the way it is written in the import statement.
   // eg. 'jquery'
   'vue',
+  '@seatsio/seatsio-types'
 ];
 
 // UMD/IIFE shared settings: output.globals
@@ -90,6 +92,7 @@ if (!argv.format || argv.format === 'es') {
       exports: 'named',
     },
     plugins: [
+      typescript(),
       replace(baseConfig.plugins.replace),
       ...baseConfig.plugins.preVue,
       vue(baseConfig.plugins.vue),
@@ -124,6 +127,7 @@ if (!argv.format || argv.format === 'iife') {
       globals,
     },
     plugins: [
+      typescript(),
       replace(baseConfig.plugins.replace),
       ...baseConfig.plugins.preVue,
       vue(baseConfig.plugins.vue),
