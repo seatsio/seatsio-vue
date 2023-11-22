6953413741a25630eb1499c7f6012b6df26c0d3c
Morten Devold <morten@seats.io>
2023-11-22 15:02:01 +0100

    Type additions

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,17 @@
-import type { ChartDesignerConfigOptions, ChartRendererConfigOptions, EventManagerConfigOptions, Region } from '@seatsio/seatsio-types'
+import type { ChartDesigner, ChartDesignerConfigOptions, ChartRendererConfigOptions, EventManager, EventManagerConfigOptions, Region, SeatingChart, Seatsio } from '@seatsio/seatsio-types'
 
 export * from '@seatsio/seatsio-types'
 
 export type SeatsioConfig = ChartRendererConfigOptions | EventManagerConfigOptions | ChartDesignerConfigOptions
-export type SeatsioEmbeddableProps = {
+
+export interface SeatsioEmbeddableProps {
   chartJsUrl?: string
-  id: string
+  id?: string
   region: Region
-  onRenderStarted?: (chart: any) => void
+  onRenderStarted?: (chart: SeatsioChartInstance) => void
 }
 
+export type SeatsioChartInstance = SeatingChart | EventManager | ChartDesigner
 export type SeatsioSeatingChartProps = SeatsioEmbeddableProps & ChartRendererConfigOptions
 export type SeatsioEventManagerProps = SeatsioEmbeddableProps & EventManagerConfigOptions
 export type SeatsioChartDesignerProps = SeatsioEmbeddableProps & ChartDesignerConfigOptions
